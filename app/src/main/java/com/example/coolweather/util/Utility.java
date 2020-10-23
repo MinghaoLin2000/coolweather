7570968733b707511c1e6543c7924e49eaf6a06d
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-10-23 11:34:43 +0800

    123

--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -6,6 +6,8 @@ import android.text.TextUtils;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.Country;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -70,6 +72,19 @@ public class Utility {
             return true;
         }
         return false;
+    }
+    //解析天气，将json数据解析成Weather实体类
+    public static Weather handleWeatherResponse(String response)
+    {
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            String weatherContent=jsonObject.toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 
 
